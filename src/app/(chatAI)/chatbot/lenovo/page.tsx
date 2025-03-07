dec85c2e2c07aa1bfef1e5ccaabdf0d5f17e3f6f
good8629 <good8629@gmail.com>
2025-03-07 16:12:41 +0900

    레노버 수정

--- src/app/(chatAI)/chatbot/lenovo/page.tsx
+++ src/app/(chatAI)/chatbot/lenovo/page.tsx
@@ -73,8 +73,15 @@ export default function Home() {
  
     return(
         <>
-            <div>
-                아무것도 없음
+            <div className={styles.pc_body}>
+                <div className={styles.pc_container}>
+                    <div className={styles.pc_left}>
+                        {modelInfo()}
+                    </div>
+                    <div className={styles.pc_right}>
+                        <iframe src="/" width="393" height="700" className={styles.pc_background}></iframe>
+                    </div>
+                </div>
             </div>
         </>
     )
