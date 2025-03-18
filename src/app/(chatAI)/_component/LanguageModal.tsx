adadfda872dc3af52311f66b05423affc9e96b29
good8629 <good8629@gmail.com>
2025-03-18 18:08:09 +0900

    수정

--- src/app/(chatAI)/_component/LanguageModal.tsx
+++ src/app/(chatAI)/_component/LanguageModal.tsx
@@ -2,9 +2,8 @@
 
 import styles from "./talk.module.css";
 import Image from 'next/image';
-import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useTogievelabsModalStore } from "@/store/togievelabs/useModalStore";
 
 export default function LanguageModal() {
