5f5eb436b82c9bafea1322287efa170a8604a14b
good8629 <good8629@gmail.com>
2025-03-18 17:57:50 +0900

    서버랜더링 수정

--- src/app/(chatAI)/chatbot/ysu/mobile/page.tsx
+++ src/app/(chatAI)/chatbot/ysu/mobile/page.tsx
@@ -2,15 +2,18 @@
 
 "use client";
 
+import { Suspense } from "react";
 import Talk from '@/app/(chatAI)/_component/YsuTalk';
 import styles from '@/app/(chatAI)/_component/ysuTalk.module.css';
 
 export default function Home() {
     return(
         <>
-            <div className={styles.body}>
-                <Talk></Talk>
-            </div>
+            <Suspense fallback={<p>Loading...</p>}>
+                <div className={styles.body}>
+                    <Talk></Talk>
+                </div>
+            </Suspense>
         </>
     )
 }
